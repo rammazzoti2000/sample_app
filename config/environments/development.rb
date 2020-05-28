6d5050b7ac48b0cddeac18776873282cae5775a6
Alexandru Bangau <rammazzoti2000@gamil.com>
2020-05-28 16:44:26 +0200

    Add account activations

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,6 +12,11 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :test
+  host = 'localhost:3000'                     # Local server
+  config.action_mailer.default_url_options = { host: host, protocol: 'http' }
+
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
